fee307b38d6b55682f3db51ed35448857fa9ef5b
YohrggenOlaf <1725110905@utectulancingo.edu.mx>
2026-05-13 14:48:40 +0000

    CREATED Ejercicios Hola_mundo.py

---
+++ Ejercicios/Semana_02/Hola_mundo.py
@@ -0,0 +1,13 @@
+class NombreClase:
+    
+    def __nit__(self):
+        print("Constructor")
+
+        
+	def metodoUno(self):
+        print("Metodo uno")
+            
+            
+        
+nombre_objeto = NombreClase()
+nombre_objeto.metodoUno()

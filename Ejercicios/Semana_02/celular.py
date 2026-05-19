cf4e179b788a38c09e65090a3145ea8ea429892d
YohrggenOlaf <1725110905@utectulancingo.edu.mx>
2026-05-19 19:54:36 +0000

    UPDATE actividades

--- Ejercicios/Semana_02/celular.py
+++ Ejercicios/Semana_02/celular.py
@@ -12,26 +12,15 @@ class Celular:
         self.memoria = memoria
         self.botones = botones
 
-    def conectar_hardware_software(self):
-    def ejecutar_programas(self):
-    def verificar_seguridad(self):
-    def modificar_parametros(self):
-    def teclear_informacion(self):
-    def visualizar_contenido(self):
-    def conectar_dispositivos(self):
-    def tomar_captura(self):
-    def almacenar_informacion(self):
-    def interactuar_fisicamente(self):
-
-    print(f"El sistema operativo {self.sistema_operativo} está conectando el hardware con el software.")
-    print(f"Ejecutando aplicaciones: {self.aplicaciones}. Son los programas ejecutables del dispositivo.")        
-    print(f"Seguimiento de seguridad activo a través del estado de actualizaciones: {self.actualizaciones}.")
-    print(f"Accediendo al menú de {self.configuracion} para modificar los parámetros del dispositivo.")       
-    print(f"El teclado {self.teclado} nos permite teclear y agregar información de texto.")
-    print(f"La pantalla {self.pantalla} permite visualizar todo el contenido multimedia.")
-    print(f"Los puertos de tipo {self.puertos} permiten la conexión con otros dispositivos físicos.")
-    print(f"La cámara de {self.camara} toma capturas de lo que se le muestra en el entorno.")
-    print(f"La memoria {self.memoria} almacena de forma segura la información que se indica.")
-    print(f"Los botones de tipo {self.botones} nos permiten cierta interacción física con el equipo.")
+        print(f"El sistema operativo {self.sistema_operativo} está conectando el hardware con el software.")
+        print(f"Ejecutando aplicaciones: {self.aplicaciones}. Son los programas ejecutables del dispositivo.")        
+        print(f"Seguimiento de seguridad activo a través del estado de actualizaciones: {self.actualizaciones}.")
+        print(f"Accediendo al menú de {self.configuracion} para modificar los parámetros del dispositivo.")       
+        print(f"El teclado {self.teclado} nos permite teclear y agregar información de texto.")
+        print(f"La pantalla {self.pantalla} permite visualizar todo el contenido multimedia.")
+        print(f"Los puertos de tipo {self.puertos} permiten la conexión con otros dispositivos físicos.")
+        print(f"La cámara de {self.camara} toma capturas de lo que se le muestra en el entorno.")
+        print(f"La memoria {self.memoria} almacena de forma segura la información que se indica.")
+        print(f"Los botones de tipo {self.botones} nos permiten cierta interacción física con el equipo.")
 
 mi_celular = Celular("Android 14","WhatsApp, GitHub Mobile, Termux","Al día (Parche de Mayo)","Ajustes del Sistema","Gboard Digital","AMOLED 6.7 pulgadas","USB Type-C","50 Megapíxeles","128 GB NVMe","Volumen y Encendido")
---
+++ Ejercicios/Semana_02/silla.py
@@ -0,0 +1,25 @@
+class Silla:
+        def __init__(self,forma_asiento, altura, textura, color, material, dureza, forma_respaldo, ensamble, estilo, antiguedad):
+                self.forma = forma_asiento
+                self.altura = altura
+                self.textura = textura
+                self.color = color
+                self.material = material
+                self.dureza = dureza
+                self.respaldo = forma_respaldo
+                self.ensamble = ensamble
+                self.estilo = estilo
+                self.antiguedad = antiguedad
+
+                print(f"La forma del asiento es: {forma_asiento}")
+                print(f"La altura de la silla es: {altura}")
+                print(f"La textura de la silla es: {textura}")
+                print(f"El color de la silla es: {color}")
+                print(f"La silla esta hecha de: {material}")
+                print(f"La silla cuenta con el siguiente valor de dureza: {dureza}")
+                print(f"La forma del respaldo es: {forma_respaldo}")
+                print(f"La silla esta ensamblada con: {ensamble}")
+                print(f"La silla tiene un estilo: {estilo}")
+                print(f"La silla tiene una antiguedad de: {antiguedad}")
+
+mi_gamer = Silla("Rectangular","35 cm","Terciopelo","Guinda","Madera","30–40 ILD","Ovaldado","Tornillos","Rustica","6 meses")
